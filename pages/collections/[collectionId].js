632dc100dd2a10f4e46f2b7d50b9c26bae8d04df
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-10-13 19:25:25 +0100

    [NFTCard]: create nft card object; import react-icons

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -5,7 +5,10 @@ import Link from 'next/link';
 import { useWeb3 } from "@3rdweb/hooks";
 import { client } from '../../lib/sanityClient'
 import { ThirdwebSDK } from '@3rdweb/sdk'
-
+import Header from '../../components/Header'
+import { CgWebsite } from 'react-icons/cg'
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { HiDotsVertical } from 'react-icons/hi'
 
 const style = {
     bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
@@ -112,9 +115,8 @@ const Collection = () => {
     console.log(router.query)
     console.log(router.query.collectionId)
     return (
-        <Link href="/">
-            <h2>{router.query.collectionId}</h2>
-        </Link>
+        <></>
+
     )
 }
 
